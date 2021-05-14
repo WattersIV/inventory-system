150007a4a32d72ffa806ac314aaf94fdb8e2b308
Bill Watters <billywatters13@gmail.com>
2021-05-14 18:28:29 -0400

    Add controllers file structure

---
+++ server/controllers/store/export.js
@@ -0,0 +1,6 @@
+import * as r from "./read";
+import * as c from "./create";
+import * as u from "./update";
+import * as d from "./delete";
+
+export { r, u, c, d };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
-
 const router = express.Router()
+import * as book from '../controllers/book/export'
+import * as store from '../controllers/store/export'
+import * as store_book from '../controllers/book_store/export'
 
 module.exports = router
